a93a13f2a8ae14b4613d04a7dd5c5c461ca4a586
Shashi Ji <shashikumarkushwaha3@gmail.com>
2023-07-25 18:27:34 +0530

    change class of Spinner into func

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,13 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Spinner extends Component {
-  render() {
-    return (
-      <div className="spinner ">
-        <div className="spinner-border" role="status">
-          <span className="visually-hidden ">Loading...</span>
-        </div>
+const Spinner = () => {
+  return (
+    <div className="spinner ">
+      <div className="spinner-border" role="status">
+        <span className="visually-hidden ">Loading...</span>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+export default Spinner;
